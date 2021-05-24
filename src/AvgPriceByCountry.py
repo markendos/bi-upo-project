bdf9341a3f9e2d64bef373dfa7556d62b2130f34
Alvaro <alvaronavarromora@gmail.com>
2021-05-24 19:04:21 +0200

    add avg price by country

---
+++ src/AvgPriceByCountry.py
@@ -0,0 +1,27 @@
+from DBConnection import DBConnection
+import plotly.graph_objs as go
+import numpy as np
+
+class AvgPriceByCountry:
+    plot = None
+
+    def __init__(self) -> None:
+        pass
+
+    def getPlot(self):
+        db = DBConnection.get_instance()
+        cursor = db.cursor()
+        cursor.execute("select country, avg(price) from bi_solutions.shop group by country")
+        result = cursor.fetchall()
+        c = []
+        p = []
+        for row in result:
+            c.append(row[0])
+            p.append(row[1])
+            
+        data = [go.Bar(
+            x = c,
+            y = p
+        )]
+        fig = go.Figure(data=data)
+        return fig
--- src/mainAlvaro.py
+++ src/mainAlvaro.py
@@ -3,6 +3,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from AvgPriceByCountry import AvgPriceByCountry
 from ProductsBoughtTogether import ProductsBoughtTogether
 from BestSalesByMonth import BestSalesByMonth
 
@@ -10,12 +11,15 @@ numberOfPBT = 5
 def definirLayout():
     pbt = ProductsBoughtTogether()
     bsbm = BestSalesByMonth()
+    apbc = AvgPriceByCountry()
     app.layout = html.Div(children=[
     html.H1(children='Alvaro\'s Components'),
     html.H2(children='Products that are bought together often'),
     pbt.getTable(),
     html.H2(children='Best sales by month'),
-    bsbm.getTable()
+    bsbm.getTable(),
+    html.H2(children="Average price by country"),
+    dcc.Graph(id = "avg_price_country", figure=apbc.getPlot())
 ])
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
