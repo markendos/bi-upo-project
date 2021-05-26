1a9f698b60ca7550aff3837f9f7f9f40f741d6cc
lukebee <luka.bubalo@bornfight.com>
2021-05-26 21:12:54 +0200

    Docker feature done

--- src/main.py
+++ src/main.py
@@ -1,45 +1,92 @@
-from logging import PlaceHolder
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from ProductsBoughtTogether import ProductsBoughtTogether
+import plotly.graph_objects as go
 from dash.dependencies import Input, Output
-
-numberOfPBT = 5
-def definirLayout():
-    pbt = ProductsBoughtTogether()
-    app.layout = html.Div(children=[
-    html.H1(children='Sample App'),
-
-    html.Div(children='''
-        Sample App.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': ["Lunes", "Martes", "Miercoles"], 'y': [150, 200, 109], 'type': 'bar', 'name': '2015'},
-                {'x': ["Lunes", "Martes", "Miercoles"], 'y': [150, 150, 150], 'type': 'bar', 'name': '2018'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    ),
-    html.H2(children='Products that are bought together often'),
-    pbt.getTable()
-    
-])
-
-
-
+from Tendencies import Datamart as dm
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-definirLayout()
+app.layout = html.Div(children=[
+    html.Div([
+        html.Div([
+            dcc.Dropdown(
+                id='first-select',
+                options=dm.get_variables(),
+                value='',
+                placeholder="Select a variable",
+            )
+        ],
+            style={'width': '48%', 'display': 'flex', 'flex-direction': 'column'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='second-select',
+                options=dm.get_values('country'),
+                value='',
+                placeholder="Select a value",
+                multi=True,
+                disabled=True
+            ),
+        ],
+            style={'width': '48%', 'display': 'flex', 'flex-direction': 'column'}),
+    ],
+        style={'display': 'flex', 'justify-content': 'space-between', 'padding': '2rem'}),
+
+    html.Div([
+        dcc.RadioItems(
+            id='crossfilter-yaxis-type',
+            options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+            value='Linear',
+            labelStyle={'display': 'inline-block', 'padding-left': '2rem'}),
+        dcc.Graph(id='time-series'),
+    ],
+        style={'width': '100%', 'display': 'flex', 'flex-direction': 'column'}),
+])
+
+
+def create_time_series(data, axis_type, variable):
+    fig = go.Figure()
+    for d in data:
+        fig.add_trace(go.Scatter(x=d['x'], y=d['y'], mode='lines+markers', name=d['name']))
+
+    fig.update_yaxes(type='linear' if axis_type == 'Linear' else 'log')
+    if variable == '':
+        variable = 'none'
+    fig.update_layout(title="Tendency chart ({})".format(variable), xaxis_title="Date", yaxis_title="Frequency",
+                      margin={'l': 20, 'b': 30, 'r': 10, 't': 40})
+
+    return fig
+
+
+@app.callback(
+    Output('time-series', 'figure'),
+    [Input('first-select', 'value'),
+     Input('second-select', 'value'),
+     Input('crossfilter-yaxis-type', 'value')])
+def update_timeseries(variable, value, yaxis_type):
+    data = dm.get_plot_data(variable, value)
+    return create_time_series(data, yaxis_type, variable)
+
+
+@app.callback(
+    Output('second-select', 'options'),
+    Output('second-select', 'value'),
+    [Input('first-select', 'value')])
+def update_values_selector(value):
+    if value in (None, ''):
+        return list(), ''
+    else:
+        return dm.get_values(value), ''
+
+
+@app.callback(
+    Output('second-select', 'disabled'),
+    [Input('first-select', 'value')])
+def set_values_selector_state(value):
+    return value in (None, '')
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', port=8080, debug=True)
--- src/mainLuka.py
+++ src/mainLuka.py
@@ -97,4 +97,4 @@ def update_gdp_graph(value, yaxis_type):
     return create_gdp_graph(data, yaxis_type, predictionForNextYear)
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8080, debug=True)
+    app.run_server(debug=True)
